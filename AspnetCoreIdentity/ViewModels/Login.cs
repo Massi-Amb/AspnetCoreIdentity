1ef4d95f686a13f37d2486fadf20ae7a775c7c75
Abdoulaye Massi Bah <dambubah@gmail.com>
2023-03-16 18:24:30 -0400

    Add project files.

---
+++ AspnetCoreIdentity/ViewModels/Login.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AspnetCoreIdentity.ViewModels
+{
+    public class Login
+    { /// <summary>
+    /// Data Annotations needed to annotate the required attributes!
+    /// </summary>
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        public bool RememberMe { get; set; }
+    }
+}
---
+++ AspnetCoreIdentity/ViewModels/Register.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AspnetCoreIdentity.ViewModels
+{
+    public class Register
+    {
+        //system.Data Annotations
+
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare(nameof(Password), ErrorMessage ="Password and confirmation did not match")]
+        public string ConfirmPassword { get; set; }
+
+    }
+}
